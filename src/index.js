6bc1e22aedd38b278f31c0acdec5cceb65ff9b61
il3ven <vaibhavchanana@yahoo.co.in>
2022-02-13 15:37:25 +0530

    setup frontend

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+
+const app = document.getElementById("app");
+ReactDOM.render(<App />, app);
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
